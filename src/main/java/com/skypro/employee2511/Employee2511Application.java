309ccdf55e618e61243728995fb3fd17a769b602
see1rg <see1rg@mail.ru>
2022-11-25 15:34:54 +0300

    Initial commit

---
+++ src/main/java/com/skypro/employee2511/Employee2511Application.java
@@ -0,0 +1,13 @@
+package com.skypro.employee2511;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Employee2511Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Employee2511Application.class, args);
+	}
+
+}
